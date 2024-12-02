9a595e9693585a1b41f9d87a33a5740c08492995
Bogich80 <bogich80@gmail.com>
2024-12-02 21:31:24 +0100

    1/1

---
+++ 1/1_1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import re
+import math
+
+data_file = open(r'C:\Users\Bogich\PycharmProjects\AdventOfCode\1\data_1.txt', 'r')
+data_lines = np.array(data_file.readlines())
+first_column_data = []
+second_colum_data = []
+for row in data_lines:
+    row_data = re.findall(r'\d+', row)
+    first_column_data.append(int(row_data[0]))
+    second_colum_data.append(int(row_data[1]))
+first_column_data.sort()
+second_colum_data.sort()
+diff = 0
+for i in range(len(first_column_data)):
+    x = math.fabs(first_column_data[i] - second_colum_data[i])
+    diff += x
+print(diff)
